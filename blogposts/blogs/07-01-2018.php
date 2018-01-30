c70e89e0e813a946bb3fb6c77d0deb6b694556f4
Abby Acourt <abigail.acourt@bglgroup.co.uk>
2018-01-30 14:00:19 +0000

    Amended Font and Scroll Issue

--- blogposts/blogs/07-01-2018.php
+++ blogposts/blogs/07-01-2018.php
@@ -14,7 +14,7 @@
     <link rel='stylesheet' href='/../css/icons.css'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body onload="javascript:window.scrollTo(0,0);">
     <?php include ('../../header.html'); ?>
     <center>
         <p class="blogTitle"> 2018 Blog Overhaul</p>
